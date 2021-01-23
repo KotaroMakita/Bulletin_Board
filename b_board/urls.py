94cc32489cd6117391b433d7e199a3dc85098177
kotaro_makita <makita2038@gmail.com>
2021-01-23 18:33:45 +0900

    CreateViewとDetailViewを書いた

--- b_board/urls.py
+++ b_board/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import B_BoardList
+from .views import B_BoardList, B_BoardDetail, B_BoardCreate
 
 urlpatterns = [
-    path('list/', B_BoardList.as_view()),
+    path('list/', B_BoardList.as_view(), name='list'),
+    path('detail/<int:pk>',B_BoardDetail.as_view(), name='detail'),
+    path('create/', B_BoardCreate.as_view(), name='create'),
 ]
--- b_board/views.py
+++ b_board/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import B_BoardModel
+from django.urls import reverse_lazy
 
 # Create your views here.
+
 
 class B_BoardList(ListView):
     template_name = 'list.html'
     model = B_BoardModel
+
+
+class B_BoardDetail(DetailView):
+    template_name = 'detail.html'
+    model = B_BoardModel
+
+
+class B_BoardCreate(CreateView):
+    template_name = 'create.html'
+    model = B_BoardModel
+    fields = ('title', 'comment', 'good')
+    success_url = reverse_lazy('list')
