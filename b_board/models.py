a26da1b57ab973aa259e1fa37ae76a167222c683
kotaro_makita <makita2038@gmail.com>
2021-01-23 10:57:13 +0900

    DB作成のためmodel.pyでtitle,comment,good項目を設定した

--- b_board/models.py
+++ b_board/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class B_BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    comment = models.TextField()
+    good = models.IntegerField()
