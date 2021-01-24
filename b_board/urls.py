ec916b13057e813ec76e8c0d529bfc606bedab5f
kotaro_makita <makita2038@gmail.com>
2021-01-24 23:08:59 +0900

    各ボタンを消した。タイトルに紐付くコメントに関して考える

--- b_board/urls.py
+++ b_board/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import B_BoardList, B_BoardDetail, B_BoardCreate
 
 urlpatterns = [
-    path('list/', B_BoardList.as_view(), name='list'),
+    path('', B_BoardList.as_view(), name='list'),
     path('detail/<int:pk>',B_BoardDetail.as_view(), name='detail'),
     path('create/', B_BoardCreate.as_view(), name='create'),
 ]
